2a1a5bda8ad9417e206062581358740f097894b8
Piotr Szkolnik <szkolnik.piotr@gmail.com>
2023-05-25 14:40:05 +0100

    msg

--- SETUP-push-pull.R
+++ SETUP-push-pull.R
@@ -6,6 +6,6 @@ use_git_config(
 )
   
 usethis::create_github_token()
-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
 
-gitcreds::gitcreds_set(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)
+
+gitcreds::gitcreds_set()
