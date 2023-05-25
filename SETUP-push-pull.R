f4501be701e8a5e778999f8ee74fda18450c1131
Piotr Szkolnik <szkolnik.piotr@gmail.com>
2023-05-25 14:32:34 +0100

    msg

--- SETUP-push-pull.R
+++ SETUP-push-pull.R
@@ -1,1 +1,11 @@
-jhgjh
+library(usethis)
+
+use_git_config(
+  user.name = "thor4fun",
+  user.email = "szkolnik.piotr@gmail.com"
+)
+  
+usethis::create_github_token()
+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
+
+gitcreds::gitcreds_set(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)
